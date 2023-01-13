4bbba7ad670dcc2fb0a5da77f86c1d59879d9b3a
wild-arctic-fox <amusing.arctic.fox@gmail.com>
2023-01-13 17:31:17 +0200

    Generate random integer function created

---
+++ index.js
@@ -0,0 +1,8 @@
+const generateRandomInteger = (min, max) => {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  // The maximum is exclusive and the minimum is inclusive
+  return Math.floor(Math.random() * (max - min) + min);
+}
+
+module.exports = generateRandomInteger
